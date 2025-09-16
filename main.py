d2a4e706d1144a46dce90aa59d6462340e391d19
kakurokan <leokakali7@gmail.com>
2025-09-16 15:22:39 +0100

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,103 @@
+DIGITOS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+DIGITOS_PARA_VALOR = {ch: i for i, ch in enumerate(DIGITOS)}
+
+def para_decimal_fracionario(fracao, base_inicial):
+    soma = 0
+
+    pot = base_inicial
+    for d in fracao:
+        soma += DIGITOS_PARA_VALOR[d] / pot
+        pot *= base_inicial
+
+    return soma
+
+
+def para_decimal_inteiro(numero, base_inicial):
+    soma = 0
+    n = len(numero)
+    i = 1
+    for d in numero:
+        soma += DIGITOS_PARA_VALOR[d] * (base_inicial ** (n - i))
+        i += 1
+    return soma
+
+
+def decimal_base_fracionario(fracao, base_final):
+    resultado = ""
+    while fracao > 0 and len(resultado) < 8:  # Limitar a 8 dígitos para evitar loops infinitos
+        fracao *= base_final
+        resultado += DIGITOS[int(fracao)]
+        fracao -= int(fracao)
+
+    return resultado
+
+
+def decimal_base_inteiro(numero, base_final):  # De decimal para qualquer outra base
+    resto = numero % base_final
+    quociente = numero // base_final
+    if quociente >= base_final:
+        return decimal_base_inteiro(quociente, base_final) + DIGITOS[resto]
+    else:
+        if quociente == 0:
+            return DIGITOS[resto]
+        return DIGITOS[quociente] + DIGITOS[resto]
+
+
+def base_para_base(numero, base_inicial, base_final):
+    if numero == "0":
+        return 0
+
+    complemento = ""
+    if numero[0] == '-':
+        numero = numero[1:]
+        complemento = '-'
+
+    if '.' in numero:
+        inteiro, frac = numero.split(".")
+    elif ',' in numero:
+        inteiro, frac = numero.split(",")
+    else:
+        inteiro, frac = numero, ""
+
+    inteiro_decimal = para_decimal_inteiro(inteiro, base_inicial)
+    inteiro_final = decimal_base_inteiro(inteiro_decimal, base_final)
+    if frac:
+        frac_decimal = para_decimal_fracionario(frac, base_inicial)
+        frac_final = decimal_base_fracionario(frac_decimal, base_final)
+        return complemento + str(inteiro_final) + '.' + str(frac_final)
+
+    return complemento + str(inteiro_final)
+
+
+def main():
+    print(DIGITOS_PARA_VALOR)
+
+    is_running = True
+
+    while is_running:
+        print("*****************************************************")
+        print("Bem vindo ao sistema de conversão de bases numéricas!")
+        print("*****************************************************")
+
+        numero = input("Digite o numero que deseja converter: ")
+        try:
+            base_inicial = int(input("Digite a base do numero inserido (2-62): "))
+            for c in numero:
+                if c != '-' and c != '.' and c != ',' and DIGITOS_PARA_VALOR[c] >= int(base_inicial):
+                    raise ValueError("O numero não é valido para a base escolhida")
+
+            base_final = int(input("Digite a base para qual deseja converter (2-62): "))
+            print(f"Resultado: {base_para_base(numero, base_inicial, base_final)}")
+
+            print("\nDeseja realizar outra conversão?")
+            continuar = input("Digite 's' para sim ou qualquer outra tecla para sair: ").lower()
+            if continuar != 's':
+                is_running = False
+            print("\n")
+        except ValueError:
+            print("Valor incompatível! Tente novamente.\n")
+
+    print("Obrigado por usar o conversor de bases numéricas!")
+
+
+main()
